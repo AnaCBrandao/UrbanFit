8272a5041cb5c220f01f4bc494c15b7376a5bdb7
anaBrandao <anacarolini633@gmail.com>
2025-09-29 21:51:06 -0300

    Corrige pasta frontend (removendo submódulo)

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'events' },
+  {
+    path: 'events',
+    loadChildren: () => import('./events/events.module').then(m => m.EventsModule)
+  }
+];
